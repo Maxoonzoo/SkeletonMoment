00d591d2509ed345421e27276173b10b93632d92
Maxoonzoo <maxoonzoo1@gmail.com>
2023-05-22 11:41:17 +0300

    fibonachi created

---
+++ test/test/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace practiceTask
+{
+    class program
+    {
+    public static void Main()
+        {
+            int a = 1;
+            int b = 1;
+            int c = 1;
+            int[] nums = new int[40];
+            nums[0] = a;
+            nums[1] = b;
+            for (int i = 2; i < 40; i++)
+            {
+                c = a + b;
+                nums[i] = c;
+                a = b;
+                b = c;
+            }
+            Console.WriteLine(string.Join(", ", nums));
+        }
+    }
+}
